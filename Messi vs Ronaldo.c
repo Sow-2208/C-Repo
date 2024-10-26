ec456901516ec1cf2a596aa12c7166b71a463486
Sowjanya <sowjanyaetamsetti@gmail.com>
2024-10-26 19:49:09 +0530

    Uploading code: Messi vs Ronaldo.c

---
+++ Messi vs Ronaldo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,x,y;
+    scanf("%d %d %d %d",&a,&b,&x,&y);
+    if(((a*2)+b)>((x*2)+y))
+    printf("Messi");
+    else if(((a*2)+b)<((x*2)+y))
+    printf("Ronaldo");
+    else if(((a*2)+b)==((x*2)+y))
+    printf("Equal");
+}
