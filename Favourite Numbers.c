570799283e0c379a062c36d2d5e1782840bfad34
Sowjanya <sowjanyaetamsetti@gmail.com>
2024-10-27 07:28:09 +0530

    Uploading code: Favourite Numbers.c

---
+++ Favourite Numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%2==0&&a%7==0)
+    printf("Alice");
+    else if(a%2==1&&a%9==0)
+    printf("Bob");
+    else
+    printf("Charlie");
+}
