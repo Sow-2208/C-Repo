083db231c742fddc07a1bea644288f277e687552
Sowjanya <sowjanyaetamsetti@gmail.com>
2024-11-11 17:26:40 +0530

    Uploading code: Parliament.c

---
+++ Parliament.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,x;
+    scanf("%d %d",&n,&x);
+    if((n+1)/2<=x)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
