09cf8f8c12c8fef06381453eb65e86c2c6a55148
Sowjanya <sowjanyaetamsetti@gmail.com>
2024-11-17 10:19:39 +0530

    Uploading code: Sum of digits in a String.c

---
+++ Sum of digits in a String.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    scanf("%[^\n]s",&str);
+    int sum=0;
+    for(int i=0;str[i]!='\0';i++)
+    {
+        if(str[i]>=48&&str[i]<=57)
+        {
+        sum+=(str[i]-'0');
+        }
+    }
+    printf("%d",sum);
+}
