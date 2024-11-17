d2b40eabd15fc2de1de8d470f0d42b0a74c8759c
Sowjanya <sowjanyaetamsetti@gmail.com>
2024-11-17 10:40:57 +0530

    Uploading code: Uppercase Letters in a String.c

---
+++ Uppercase Letters in a String.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    scanf("%[^\n]s",&str);
+    int count=0;
+    for(int i=0;str[i]!='\0';i++)
+    {
+        if(str[i]>='A'&&str[i]<='Z')
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}
