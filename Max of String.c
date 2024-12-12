f039c36d5431603d881fa2d450ef13690ee2384f
Sowjanya <sowjanyaetamsetti@gmail.com>
2024-12-12 13:11:31 +0530

    Uploading code: Max of String.c

---
+++ Max of String.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    scanf("%[^\n]s",&str);
+    int max=str[0];
+    for(int i=0;str[i]!='\0';i++)
+    {
+        if(str[i]>max)
+        max=str[i];
+    }
+    printf("%c",max);
+}
