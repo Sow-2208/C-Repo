495ad647f54dbc486f47fe0b3aec33a4fc6bbea0
Sowjanya <sowjanyaetamsetti@gmail.com>
2024-12-13 15:10:37 +0530

    Uploading code: Strictly ODD.c

---
+++ Strictly ODD.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int ar[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    int co1=0;
+    for(int i=0;i<n;i++)
+    {
+        if(ar[i]%2==1)
+        {
+            co1++;
+        }
+    }
+    int co=0;
+    for(int i=0;i<n;i++)
+    {
+        if(ar[i]%2!=0 && i%2!=0)
+        {
+            co++;
+        }
+    }
+    if(co1==co) printf("True");
+    else printf("False");
+}
